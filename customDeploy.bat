6e7dde0f42683afc0aeffc15be115192dcb7cace
fedema92 <f.mauri92@gmail.com>
2019-04-23 17:28:12 +0200

    new auto deploy procedure

--- customDeploy.bat
+++ customDeploy.bat
@@ -6,14 +6,14 @@ ECHO RUN: npm install
 call npm install
 
 ECHO ------------------------
-ECHO Npm install DONE succesfully
+ECHO Npm install DONE successfully
 ECHO ------------------------
 
 ECHO RUN: npm run build for production env
 call npm run build
 
 ECHO ------------------------
-ECHO Npm run build DONE succesfully
+ECHO Npm run build DONE successfully
 ECHO ------------------------
 
 ECHO RUN: node beforePublishCustom script for change version and module.exports
@@ -27,13 +27,18 @@ ECHO ------------------------
 set /P c=You want to publish the package in NPM repo [Y/N]?
 if /I "%c%" EQU "Y" goto :somewhere
 if /I "%c%" EQU "N" goto :somewhere_else
-goto :choice
 
 
 :somewhere
 
 echo "Publish your script to NPM"
-( cd package/react-data-grid ; npm publish )
+cd packages\react-data-grid
+call npm publish
+cd..
+cd..
+ECHO ------------------------
+ECHO Package published successfully
+ECHO ------------------------
 
 
 :somewhere_else
